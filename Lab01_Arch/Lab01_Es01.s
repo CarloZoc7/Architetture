2787da7b2d3d83570af7f1623e316ffec2303060
carlo <carlo@carlo-MacBookPro>
2019-10-18 18:23:05 +0200

    male male

--- Lab01_Arch/Lab01_Es01.s
+++ Lab01_Arch/Lab01_Es01.s
@@ -3,14 +3,14 @@
 ; last update:  16 October 2019                   ;
 
 .data
-VectorA:  	.byte  	1,1,1,1,1,1,1,1,1,1
-			.byte   2,2,2,2,2,2,2,2,2,2
-			.byte   3,3,3,3,3,3,3,3,3,3
-			.byte  	4,4,4,4,4,4,4,4,4,4
-			.byte	5,5,5,5,5,5,5,5,5,5
+VectorA:  	.byte  	1,2,3,4,5,6,7,8,9,10
+			.byte   21,22,23,24,25,26,27,28,29,30	
+			.byte   31,32,33,34,35,36,37,38,39,40
+			.byte  	41,42,43,44,45,46,47,48,49,50
+			.byte	51,52,53,54,55,56,57,58,59,60
 
 
-VectorB:  	.byte  	10,10,10,10,10,10,10,10,10,10
+VectorB:  	.byte  	11,12,13,14,15,16,17,18,19,20
 			.byte   20,20,20,20,20,20,20,20,20,20
 			.byte   30,30,30,30,30,30,30,30,30,30
 			.byte  	40,40,40,40,40,40,40,40,40,40
@@ -18,14 +18,14 @@ VectorB:  	.byte  	10,10,10,10,10,10,10,10,10,10
 
 Result:		.space 50	 
 
-Max:	.byte 0
-Min:	.byte 255
+Max:	.space 1
+Min:	.space 1
 
 .text         
 
 start:  	
 			daddui r2, r0,	0 ; indice del vettore a 0
-			daddui r3, r0,  49
+			daddui r3,r0, 50
 
 cycle:
 			lb r4, VectorA(r2)
@@ -36,19 +36,20 @@ cycle:
 			bne r2, r3, cycle
 
 			daddui r2, r0, 0
-			daddui r3, r0, 49
+			daddui r3,r0, 50
 
-			lb r6, Max(r0)
-			lb r5, Min(r0)
-
+			daddui r6, r0, 0 	; max
+			daddui r5, r0, 255	; min
+			
 cycle2:	
 			lb r4, Result(r2)
-			slt r1, r6, r4	; if(max<Result[r2]) r1 = 1 else r1 = 0
-			bne r1, r0, max; condizione verificata r1 != r0
+			slt r1, r4, r6	; if(Result[r2]<max) r1 = 1 else r1 = 0
+			beq r1, r0, max ; condizione verificata r1==r0
+continue1:
 			slt r1, r4, r5	; if(Result[r2]<min) r1 = 1 else r1 = 0 
-			bne r1, r0, min ;condizione verificata r1 != r0
+			bne r1, r0, min ; condizione verificata r1 != r0
+continue2:
 
-continue:	
 			daddui r2, r2, 1		
 			bne r2, r3, cycle2
 			sb r6, Max(r0)
@@ -58,10 +59,12 @@ continue:
 
 max:		daddui r6, r0, 0
 			dadd r6, r0, r4
-  			j continue
+  			j continue1
 
 min:		daddui r5, r0, 0
 			dadd r5, r0, r4
-			j continue
+			j continue2
+
+			nop
 
 block: j block
