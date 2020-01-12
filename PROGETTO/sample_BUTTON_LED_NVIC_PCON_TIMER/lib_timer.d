7ee74aa14e1125c2353e517e4d15d7c161c24e67
Carlo Zoccoli <carlo.zoc7@gmail.com>
2020-01-12 14:28:39 +0100

    final project

--- PROGETTO/sample_BUTTON_LED_NVIC_PCON_TIMER/lib_timer.d
+++ PROGETTO/sample_BUTTON_LED_NVIC_PCON_TIMER/lib_timer.d
@@ -1,10 +1,8 @@
 .\lib_timer.o: timer\lib_timer.c
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\lpc17xx.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm3.h
+.\lib_timer.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\lpc17xx.h
+.\lib_timer.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
 .\lib_timer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
-.\lib_timer.o: C:\Users\carlo\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.6.0\Device\Include\system_LPC17xx.h
+.\lib_timer.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\lib_timer.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\lib_timer.o: C:\Keil_v5\ARM\INC\NXP\LPC17xx\system_LPC17xx.h
 .\lib_timer.o: timer\timer.h
